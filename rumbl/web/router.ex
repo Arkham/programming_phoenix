0ad0fa93c12149553868bd3e216032fda25ec2e0
Ju Liu <ju.liu@welaika.com>
2017-03-02 12:56:12 +0000

    Add videos and admin crud page

--- rumbl/web/router.ex
+++ rumbl/web/router.ex
@@ -15,15 +15,16 @@ defmodule Rumbl.Router do
   end
 
   scope "/", Rumbl do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Rumbl do
-  #   pipe_through :api
-  # end
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
 end
